ace04f175e7e0bba3bdb4db88e28cdaf9a58d8d0
Tabea Leibl <tabea.leibl@web.de>
2022-11-21 13:50:47 +0100

    added NavMesh, spider as NavMeshAgent, changed SpiderAC script

--- Thesisprojekt/Assets/Scripts/SpiderAC.cs
+++ Thesisprojekt/Assets/Scripts/SpiderAC.cs
@@ -1,45 +1,66 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class SpiderAC : MonoBehaviour
 {
+    //references
     private Animator animator = null; //the spider animator
+    private NavMeshAgent agent = null; //NavMeshAgent of spider
+    private Transform patient = null; //the patient transform (VR player object - OVRPlayerController)
 
-    private float patientDistance = 0.0f;
-    private float nearPatient = 3.0f; //TODO set //value in which spider is near the patient 
+    [SerializeField] private float patientDistance = 0.0f; //distance of spider to patient
+    private float nearPatient = 2.0f; //value in which spider is near the patient 
+    private bool inPatientRange = false; //if true spider is near patient
 
 
-    // Start is called before the first frame update
-    private void Start()
+    private void Awake()
     {
         animator = GetComponent<Animator>();
+        agent = GetComponent<NavMeshAgent>();
+        patient = GameObject.Find("OVRPlayerController").transform;
     }
 
     private void Update()
     {
+        //update distance to patient
         CalculatePatientDistance();
+        Flee();
     }
 
 
     //method used to calculate the distance of the spider to the patient
     private void CalculatePatientDistance()
     {
-        //TODO
+        patientDistance = Vector3.Distance(this.transform.position, patient.transform.position);
+        //if in defined range - set inPatientRange true, else false
+        if (patientDistance <= nearPatient)
+            inPatientRange = true;
+        else inPatientRange = false;
     }
 
 
 
     //---------------------------MOVEMENT-----------------------------
 
+    /// <summary>
+    /// Let the spider look at a position, e.g. the patient.
+    /// </summary>
+    /// <param name="position"> The position the spider shall look at.</param>
+    private void LookAt(Vector3 position)
+    {
+        //reset y axis so spider does not turn away from the ground
+        Vector3 lookAtPosition = position;
+        lookAtPosition.y = transform.position.y;
+
+        transform.LookAt(lookAtPosition);
+    }
+
 
     //method to let spider struggle if taken by VR character
     public void Struggle()
     {
-        //TODO 
-
-        //let the spider hang in the grip
-
         //struggle animation
         animator.SetBool("isStruggling", true);
     }
@@ -48,38 +69,43 @@ public class SpiderAC : MonoBehaviour
     //method called by supervisor to let spider stop and return to idle
     public void Stop()
     {
+        //stop animations
         animator.SetBool("isWalking", false);
         animator.SetBool("isAttacking", false);
+
+        //stop movement
+        agent.SetDestination(transform.position);
     }
 
     //method called by supervisor to let spider walk towards patient
     public void WalkTowards()
     {
-        //TODO
+        //look at patient
+        LookAt(patient.position);
 
-        //Walk animation
-        animator.SetBool("isWalking", true);
         //if not already there - move into direction of patient
-        if(patientDistance > nearPatient)
+        if(!inPatientRange)
         {
+            //Walk animation
+            animator.SetBool("isWalking", true);
 
+            agent.SetDestination(patient.position);
+            while (agent.remainingDistance > agent.stoppingDistance)
+            {
+                //walk until patient position reached
+            }
+            animator.SetBool("isWalking", false);
         }
 
     }
 
 
     //method called by supervisor to let spider walk towards patient and climb onto him or her
+    //TODO
     public void WalkOnto()
     {
-        //TODO
-
-        //Walk animation
-        animator.SetBool("isWalking", true);
-        //if not already there - move into direction of patient
-        if(patientDistance > nearPatient)
-        {
-
-        }
+        //if not already there - look and move into direction of patient
+        WalkTowards();
 
         //climb onto patient
         //if hand down - climb onto hand and up the arm
@@ -89,10 +115,9 @@ public class SpiderAC : MonoBehaviour
 
 
     //method called by supervisor to let spider run away from patient and despawn 
+    //TODO
     public void Flee()
     {
-        //TODO
-
         //Walk animation
         animator.SetBool("isWalking", true);
 
@@ -100,30 +125,28 @@ public class SpiderAC : MonoBehaviour
         //if not already on floor: get down
 
         //run away
+        Vector3 dirToPatient = transform.position - patient.transform.position; //calculate current Vector3 from spider to patient
+        Vector3 newPos = transform.position + dirToPatient; //define new position for spider to run to 
+        LookAt(newPos); //look at pos
+        agent.SetDestination(newPos); //move there
+        while (agent.remainingDistance > agent.stoppingDistance)
+        {
+            //walk until flee position reached
+        }
 
         //despawn
-
+        //Destroy(gameObject);
     }
 
 
     //method called by supervisor to let spider die
+    //TODO
     public void Die()
     {
-        //TODO
-
         //if not already there drop to the ground
 
         //Death animation
         animator.SetBool("dead", true);
     }
-
-
-    //---------------------------GETTER AND SETTER-----------------------------
-
-    public float GetPatientDistance()
-    {
-        return patientDistance;
-    }
-
 
 }

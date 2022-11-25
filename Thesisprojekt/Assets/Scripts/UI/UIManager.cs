b9bc0afb7c9f77186eff8774fc4a91c208648640
Tabea Leibl <tabea.leibl@web.de>
2022-11-25 12:20:40 +0100

    added UICamera, UIManager, UIManager script, removed UI from camera culling masks except UICamera one

---
+++ Thesisprojekt/Assets/Scripts/UI/UIManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using Oculus.Interaction;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI controlWalk = null;
+    [SerializeField] private TextMeshProUGUI controlFeedback = null;
+    [SerializeField] private TextMeshProUGUI controlGrab = null;
+
+    //set texts in UI and start feedback value
+    private void Awake()
+    {
+        controlWalk.text = "Walk - Left Thumbstick";
+        controlFeedback.text = "Feedback - Right Trigger";
+        controlGrab.text = "Grab - Left/Right Grip";
+    }
+
+    //update feedback value in UI
+    private void Update()
+    {
+        //update Feedback value
+        //TODO
+        //_____ = GameManager.FeedbackValue;
+    }
+}

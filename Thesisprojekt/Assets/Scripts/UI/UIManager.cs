aabcd89140bf25a8e8e6ba425f94875458f8ff71
Tabea Leibl <tabea.leibl@web.de>
2022-12-01 13:55:12 +0100

    fixed feedback value calculation and refactored calculation value

--- Thesisprojekt/Assets/Scripts/UI/UIManager.cs
+++ Thesisprojekt/Assets/Scripts/UI/UIManager.cs
@@ -15,7 +15,7 @@ public class UIManager : MonoBehaviour
 
     //feedback
     private float lastTriggerValue = 0.0f; //the last used trigger value
-    private int valueDivider = 50; //value used for feedback value calculation
+    private float calculationValue = 0.01f; //value used for feedback value calculation
 
 
     //set texts in UI and start feedback value
@@ -38,20 +38,24 @@ public class UIManager : MonoBehaviour
     //method used by VRCharController to calculate the current feedback value in dependence of the controller trigger value
     public float CalculateCurrentFeedbackValue(float currentFeedbackValue, float triggerValue)
     {
-        Debug.Log("triggerValue: " + triggerValue + ", lastTriggerValue: " + lastTriggerValue);
-
         //increase/decrease feedback value the more trigger is pressed or released
-        if (triggerValue > lastTriggerValue)
-            currentFeedbackValue = currentFeedbackValue + triggerValue / valueDivider;
+        //if value has only slighty changed: leave it constant - return from method
+        if (Mathf.Approximately(triggerValue, lastTriggerValue))
+            return currentFeedbackValue;
+        else if (triggerValue > lastTriggerValue) //trigger is pressed more
+            if (triggerValue > 0.8f) //trigger is pressed strongly - change value more rapid
+                currentFeedbackValue += calculationValue * 2;
+            else currentFeedbackValue += calculationValue; //else normal change
         else if(triggerValue < lastTriggerValue)
-            currentFeedbackValue =  currentFeedbackValue - triggerValue / valueDivider;
+            if (triggerValue < 0.2f) //trigger is let go nearly completely - change value more rapid
+                currentFeedbackValue -= calculationValue * 2;
+            else currentFeedbackValue -= calculationValue; //else normal change
+
         //check if feedback value is between 0 and 100
         if (currentFeedbackValue > 100)
             currentFeedbackValue = 100;
         else if (currentFeedbackValue < 0)
             currentFeedbackValue = 0;
-
-        Debug.Log("CURRENTFEEDBACKVALUE: " + currentFeedbackValue);
 
         //save trigger value as last trigger value
         lastTriggerValue = triggerValue;

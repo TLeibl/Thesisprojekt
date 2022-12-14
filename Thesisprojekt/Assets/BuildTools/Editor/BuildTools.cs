f5913e82683eb55a5bb6202927f2060b88efd6c0
Tabea Leibl <tabea.leibl@web.de>
2022-12-14 18:03:12 +0100

    added BuildTools script to do builds for multiple platforms

--- Thesisprojekt/Assets/BuildTools/Editor/BuildTools.cs
+++ Thesisprojekt/Assets/BuildTools/Editor/BuildTools.cs
@@ -2,6 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
+using UnityEditor.Build;
+using UnityEditor.Build.Reporting;
+using Unity.EditorCoroutines.Editor;
 
 //script used to automatically build project for different platforms.
 //It can be controlled via Tools - BuildTools window.
@@ -86,5 +89,132 @@ public class BuildTools : EditorWindow
     {
         //header
         GUILayout.Label("Platforms to build", EditorStyles.boldLabel);
+
+        //display build targets
+        int numEnabled = 0; //how many targets are enabled to build (checked)
+
+        foreach (var target in AvailableTargets)
+        {
+            TargetsToBuild[target] = EditorGUILayout.Toggle(target.ToString(), TargetsToBuild[target]);
+
+            if (TargetsToBuild[target])
+                numEnabled++;
+        }
+
+        //do build(s)
+        if(numEnabled > 0)
+        {
+            //button to try building chosen platforms
+            string prompt = numEnabled == 1 ? "Build 1 Platform" : $"Build {numEnabled} Platforms";
+            if (GUILayout.Button(prompt))
+            {
+                List<BuildTarget> selectedTargets = new List<BuildTarget>();
+                foreach(var target in AvailableTargets)
+                {
+                    if (TargetsToBuild[target])
+                        selectedTargets.Add(target);
+                }
+
+                //start build coroutine
+                EditorCoroutineUtility.StartCoroutine(PerformBuild(selectedTargets), this);
+            }
+        }
+    }
+
+
+    //Coroutine for building chosen platforms
+    IEnumerator PerformBuild(List<BuildTarget> targetsToBuild)
+    {
+        //start sticky progress (progress remains after finishing) and keep track of it via ID
+        int buildAllProgressID = Progress.Start("Build All", "Building all selected platforms", Progress.Options.Sticky);
+        Progress.ShowDetails(); //display progress
+        yield return new EditorWaitForSeconds(1f); //wait for 1 second to finish progress
+
+        BuildTarget originalTarget = EditorUserBuildSettings.activeBuildTarget;  //keep track of original target
+
+        //loop through all targets and build each target
+        for(int targetIndex = 0; targetIndex < targetsToBuild.Count; ++targetIndex)
+        {
+            
+            var buildTarget = targetsToBuild[targetIndex]; //current target
+
+            Progress.Report(buildAllProgressID, targetIndex + 1, targetsToBuild.Count); //show progress
+
+            //start sticky progress and keep track of it via ID
+            int buildTaskProgressID = Progress.Start($"Build {buildTarget.ToString()}", null, Progress.Options.Sticky, buildAllProgressID);
+            yield return new EditorWaitForSeconds(1f); //wait for 1 second to finish progress
+
+            //perform build
+            if (!BuildIndividualTarget(buildTarget)) //failed
+            {
+                Progress.Finish(buildTaskProgressID, Progress.Status.Failed);
+                Progress.Finish(buildAllProgressID, Progress.Status.Failed);
+
+                if (EditorUserBuildSettings.activeBuildTarget != originalTarget) //if not original target anymore - switch to async target
+                    EditorUserBuildSettings.SwitchActiveBuildTargetAsync(GetTargetGroupForTarget(originalTarget), originalTarget);
+
+                yield break;
+            }
+            
+            //build was successful
+            Progress.Finish(buildTaskProgressID, Progress.Status.Succeeded);
+
+            if (EditorUserBuildSettings.activeBuildTarget != originalTarget) //if not original target anymore - switch to async target
+                EditorUserBuildSettings.SwitchActiveBuildTargetAsync(GetTargetGroupForTarget(originalTarget), originalTarget);
+
+            yield return new EditorWaitForSeconds(1f);
+        }
+
+        //coroutine reaches that point - build was successful
+        Progress.Finish(buildAllProgressID, Progress.Status.Succeeded); 
+        
+        yield return null;
+    }
+
+
+    //returns true if build was successful
+    private bool BuildIndividualTarget(BuildTarget target)
+    {
+        //the player options
+        BuildPlayerOptions options = new BuildPlayerOptions(); //the player options
+
+        //TODO eigene Szenen für jeden Build
+
+        //get list of scenes
+        List<string> scenes = new List<string>();
+        foreach (var scene in EditorBuildSettings.scenes)
+            scenes.Add(scene.path);
+
+        //configure the build
+        options.scenes = scenes.ToArray(); //the scenes to build
+        options.target = target; //build target (e.g. Windows)
+        options.targetGroup = GetTargetGroupForTarget(target);
+
+        //set location path name
+        if (target == BuildTarget.Android) //Android needs specific folder
+        {
+            string apkName = PlayerSettings.productName + ".apk";
+            options.locationPathName = System.IO.Path.Combine("Builds", target.ToString(), apkName);
+        }
+        else
+            options.locationPathName = System.IO.Path.Combine("Builds", target.ToString());
+
+        if (BuildPipeline.BuildCanBeAppended(target, options.locationPathName) == CanAppendBuild.Yes)
+            options.options = BuildOptions.AcceptExternalModificationsToPlayer; //if build can be appended - accept external modifications
+        else
+            options.options = BuildOptions.None; //default build options
+
+        //start the build
+        BuildReport report = BuildPipeline.BuildPlayer(options);
+
+        if (report.summary.result == BuildResult.Succeeded) //build was successful
+        {
+            Debug.Log($"Build for {target.ToString()} completed in {report.summary.totalTime.Seconds} seconds"); //log needed time span
+            return true;
+        }
+
+        //if method reaches that point - build failed
+        Debug.LogError($"Build for {target.ToString()} failed"); //log which build failed
+        return false;
     }
 }
